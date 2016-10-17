690263b9b3ecb848ef7c98fc2497df968a5784ce
Evan Law <evanlaw.el@gmail.com>
2016-10-16 21:15:25 -0400

    Hero finished

---
+++ Assets/__Scripts/Hero.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Hero : MonoBehaviour
+{
+    static public Hero S; // Singleton
+
+    // These fields control the movement of the ship
+    public float speed = 30;
+    public float rollMult = -45;
+    public float pitchMult = 30;
+
+    // Ship status information
+    public float shieldLevel = 1;
+
+    public bool ___________________;
+    public Bounds bounds;
+
+    void Awake()
+    {
+        S = this;  // Set the Singleton
+        bounds = Utils.CombineBoundsOfChildren(this.gameObject);
+    }
+
+    void Update()
+    {
+        // Pull in information from the Input class
+        float xAxis = Input.GetAxis("Horizontal");
+        float yAxis = Input.GetAxis("Vertical");
+
+        // Change transform.position based on the axes
+        Vector3 pos = transform.position;
+        pos.x += xAxis * speed * Time.deltaTime;
+        pos.y += yAxis * speed * Time.deltaTime;
+        transform.position = pos;
+
+        bounds.center = transform.position;
+
+        // Keep the ship constrained to the screen bounds
+        Vector3 off = Utils.ScreenBoundsCheck(bounds, BoundsTest.onScreen);
+        if (off != Vector3.zero)
+        {
+            pos -= off;
+            transform.position = pos;
+        }
+
+        // Rotate the ship to make it feel more dynamic
+        transform.rotation = Quaternion.Euler(yAxis * pitchMult, xAxis * rollMult, 0);
+    }
+}
